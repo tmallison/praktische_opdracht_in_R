bd3ea8d7d18fcf1fb48ca878f3aa286dec254031
Thomas Allison <thomas68allison@hotmail.com>
2018-12-18 14:46:48 +0100

    initial setup

---
+++ template.R
@@ -0,0 +1,33 @@
+#--------Template - Onderdeel 2 - Machine Learning - ikml (40 punten) -------------
+# Naam student: 
+# studienummer: s0123456
+
+
+# Gekozen dataset (website link). Beschrijving dataset
+# Inlezen dataset en nul-meeting (10 punten)
+# met nul meeting bedoelen we 
+# - scatterplots maken. histrogram plots
+# - dimensies van je data (aantal rijen, aantal kolommen)
+# - numerieke of categorische data
+# - missing values? 
+# - Bereik van de datapunten?
+
+
+
+
+# gekozen classifier of regressie model trainen (10 punten)
+
+
+
+
+# cross validatie of R squared (voor regressie) (10 punten)
+
+
+
+
+# visualisatie en conclusies (10 punten)
+
+
+
+
+
