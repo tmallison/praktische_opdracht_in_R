932a567ee95ba841b6effeef92438446d2c07300
Thomas Allison <thomas68allison@hotmail.com>
2019-01-11 20:33:58 +0100

    readable dataset description

--- template.R
+++ template.R
@@ -2,9 +2,16 @@
 # Naam student: Thomas Allison
 # studienummer: s1095073
 
-
-# Gekozen dataset https://archive.ics.uci.edu/ml/datasets/Student+Performance. Beschrijving dataset
-# This data approach student achievement in secondary education of two Portuguese schools. The data attributes include student grades, demographic, social and school related features) and it was collected by using school reports and questionnaires. Two datasets are provided regarding the performance in two distinct subjects: Mathematics (mat) and Portuguese language (por). In [Cortez and Silva, 2008], the two datasets were modeled under binary/five-level classification and regression tasks. Important note: the target attribute G3 has a strong correlation with attributes G2 and G1. This occurs because G3 is the final year grade (issued at the 3rd period), while G1 and G2 correspond to the 1st and 2nd period grades. It is more difficult to predict G3 without G2 and G1, but such prediction is much more useful (see paper source for more details).
+# Gekozen dataset https://archive.ics.uci.edu/ml/datasets/Student+Performance. Beschrijving dataset:
+# This data approach student achievement in secondary education of two Portuguese schools. 
+# The data attributes include student grades, demographic, social and school related features) and 
+# it was collected by using school reports and questionnaires. Two datasets are provided regarding 
+# the performance in two distinct subjects: Mathematics (mat) and Portuguese language (por). 
+# In [Cortez and Silva, 2008], the two datasets were modeled under binary/five-level classification 
+# and regression tasks. Important note: the target attribute G3 has a strong correlation with 
+# attributes G2 and G1. This occurs because G3 is the final year grade (issued at the 3rd period), 
+# while G1 and G2 correspond to the 1st and 2nd period grades. It is more difficult to predict G3 
+# without G2 and G1, but such prediction is much more useful (see paper source for more details).
 
 # Inlezen dataset en nul-meeting (10 punten)
 # met nul meeting bedoelen we 
